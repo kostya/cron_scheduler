528cd5b8bf35af0f1c53b692db991b4fcb9e6287
Konstantin Makarchev <kostya27@gmail.com>
2015-11-01 00:48:34 +0300

    fix 24

--- spec/cron_scheduler_spec.cr
+++ spec/cron_scheduler_spec.cr
@@ -4,7 +4,7 @@ describe CronScheduler do
   it "should define jobs" do
     CronScheduler.define do
       add("*/1 * * * *") { puts "every 1 minute" }
-      add("*/5 20-24 * * *") { puts "every 5 minute between 20-24 hours" }
+      add("*/5 20-23 * * *") { puts "every 5 minute between 20-23 hours" }
     end
   end
 
