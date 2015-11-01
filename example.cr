46b21c3b634c38b9c7ece96252ce0643109e7d76
Konstantin Makarchev <kostya27@gmail.com>
2015-11-01 04:11:29 +0300

    better samples

--- example.cr
+++ example.cr
@@ -4,9 +4,9 @@ require "logger"
 L = Logger.new STDOUT
 
 CronScheduler.define do
-  at("*/1 * * * *") { L.info "every 1 minute" }
-  at("*/5 20-23 * * *") { L.info "every 5 minute between 20-23 hours" }
-  at("*/2 */1 * * *") { L.info "every 2 minute every 1 hour" }
+  at("* * * * *") { L.info "every 1 minute" }
+  at("*/5 20-23 * * *") { L.info "every 5 minutes between 20-23 hours" }
+  at("*/2 */3 * * *") { L.info "every 2 minute every 3 hours" }
   at("45 * * * *") { L.info "every hour at :45" }
   at("* * * * SUN") { L.info "every 1 minute in sunday" }
 
