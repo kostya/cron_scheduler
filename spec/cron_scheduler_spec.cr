3d0dbac5546236b029691235995ffb1bf50ebc98
Konstantin Makarchev <kostya27@gmail.com>
2015-11-01 17:08:33 +0300

    add run spec

--- spec/cron_scheduler_spec.cr
+++ spec/cron_scheduler_spec.cr
@@ -3,12 +3,19 @@ require "./spec_helper"
 describe CronScheduler do
   it "should define jobs" do
     CronScheduler.define do
-      at("*/1 * * * *") { puts "every 1 minute" }
-      at("*/5 20-23 * * *") { puts "every 5 minute between 20-23 hours" }
+      at("*/1 * * * *") { }
+      at("*/5 20-23 * * *") { }
     end
   end
 
   it "custom add job" do
-    CronScheduler.at("*/1 * * * *") { puts "every 1 minute" }
+    CronScheduler.at("*/1 * * * *") { }
+  end
+
+  it "run the jobs" do
+    x = [] of Time
+    CronScheduler.at("* * * * * *") { x << Time.now }
+    sleep 2.1
+    x.size.should eq(2)
   end
 end
